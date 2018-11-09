23cd954410ee451a566edf4aabc9b32a554d9f9c
Ethan Baker <32339256+ethanbaker@users.noreply.github.com>
2018-11-09 18:58:40 -0500

    Added alert in preload()

--- game.js
+++ game.js
@@ -313,6 +313,7 @@ let gameObject = {
     },
     scene: {
         preload() { // preload: function () {}
+            alert('You are the green square, trying to collect all of the yellow coins. Don\' touch the lava, as you\'ll die. Good luck!')
             loadLocalStorage()
             this.load.image('bg', './assets/blue-background.png') // loads the sprites before the game starts to avoid lag
             this.load.image('player1', './assets/green-square.png')
